df604f46dabae2729d84f82a7e8e3342c0eca4cb
Joonbom <wachirakorn.chi@dome.tu.ac.th>
2024-10-17 21:37:59 +0700

    Final in final

--- public/js/script.js
+++ public/js/script.js
@@ -8,36 +8,42 @@ function submitLogin() {
     showErr.classList.add("error");
     showErr.innerHTML = "<label>Incorrect username or password.</label>";
 
-    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Application-Key': 'TUd528eec12c6c1ca5d523d0bb1ca13b54455f37eb7a11ca44602ef60ba13eedfec58b2f7556499739caca6ec01be25cb0'
-        },
-        body: JSON.stringify({
-            "UserName": username,
-            "PassWord": password
+    if (roles === "Select your role" || !username || !password) {
+        document.getElementById("warning").textContent = "Please Fill All The Field";
+    } else {
+        document.getElementById("warning").textContent = "";
+        fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Application-Key': 'TUd528eec12c6c1ca5d523d0bb1ca13b54455f37eb7a11ca44602ef60ba13eedfec58b2f7556499739caca6ec01be25cb0'
+            },
+            body: JSON.stringify({
+                "UserName": username,
+                "PassWord": password
+            })
         })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (roles.toLowerCase() != data.type) {
+            .then(response => response.json())
+            .then(data => {
+                if (roles.toLowerCase() != data.type) {
+                    if (document.body.getElementsByClassName("error").length == 0) {
+                        document.body.appendChild(showErr);
+                    }
+                } else {
+                    if (document.body.getElementsByClassName("error").length > 0) {
+                        document.body.removeChild(document.body.getElementsByClassName("error")[0]);
+                    }
+                    displayInfo(data);
+                }
+            })
+            .catch(error => {
                 if (document.body.getElementsByClassName("error").length == 0) {
                     document.body.appendChild(showErr);
                 }
-            } else {
-                if (document.body.getElementsByClassName("error").length > 0) {
-                    document.body.removeChild(document.body.getElementsByClassName("error")[0]);
-                }
-                displayInfo(data);
-            }
-        })
-        .catch(error => {
-            if (document.body.getElementsByClassName("error").length == 0) {
-                document.body.appendChild(showErr);
-            }
-            console.error('Error:', error)
-        });
+                console.error('Error:', error)
+            });
+    }
+
 }
 
 function displayInfo(data) {
@@ -45,7 +51,7 @@ function displayInfo(data) {
         <h2>Login success</h2>
         <div>
             <label>Student Id: ` + data.username + `</label><br><br>
-            <label>Name: `+ data.displayname_th + `</label><br><br>
+            <label>Name: ` + data.displayname_th + `</label><br><br>
             <label>Faculty: ` + data.department + `</label><br><br>
             <label>Department: ` + data.faculty + `</label><br><br>
         </div>
@@ -57,3 +63,30 @@ function displayInfo(data) {
 
     document.getElementById("wrapper").appendChild(showContent);
 }
+
+document.getElementById("username").addEventListener("input", () => {
+    const username = document.getElementById('username').value;
+    if (!username) {
+        document.getElementById("usernameErr").textContent = "require";
+    } else {
+        document.getElementById("usernameErr").textContent = "";
+    }
+})
+
+document.getElementById("password").addEventListener("input", () => {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        document.getElementById("passwordErr").textContent = "require";
+    } else {
+        document.getElementById("passwordErr").textContent = "";
+    }
+})
+
+document.getElementById("roles").addEventListener("input", () => {
+    const roles = document.getElementById('roles').value;
+    if (!roles) {
+        document.getElementById("selectErr").textContent = "require";
+    } else {
+        document.getElementById("selectErr").textContent = "";
+    }
+})
